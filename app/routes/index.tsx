964b93d44257f539bc1e139f48e314c23d2e3c9d
primayuda <aprita.primayuda@gmail.com>
2022-09-27 13:17:50 +0700

    before using PlanetScale

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,81 @@
-import type { LoaderFunction } from "@remix-run/node";
+import type { ActionFunction, LoaderFunction} from "@remix-run/node";
+import { redirect } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import { getPosts } from "~/services/posts.server";
+import { useActionData, useLoaderData } from "@remix-run/react";
+import { createPost, getPosts } from "~/services/posts.server";
 import type { Post } from '~/services/posts.server';
 import { Post as PostComponent } from '~/components/Post';
+import { PostForm } from "~/components/PostForm";
+import { CreatePost } from "~/services/validations";
 
 type LoaderData = {
   posts: Post[]
+};
+
+type ActionData = {
+  error: {
+    formError?: string[]
+    fieldError?: {
+      title?: string[]
+      body?: string[]
+    }
+  }
+  fields: {
+    title?: string
+    body?: string
+  }
 }
+
+export const action: ActionFunction = async ({request}) => {
+  const form = await request.formData();
+  const rawTitle = form.get('title');
+  const rawBody = form.get('body');
+
+  const result = CreatePost.safeParse({title: rawTitle, body: rawBody})
+
+  if (!result.success) {
+    return json({
+      error: result.error.flatten(),
+      fields: {
+        title: rawTitle,
+        body: rawBody
+      }
+    }, {status: 400})
+  }
+  await createPost({title: result.data.title ?? null, body: result.data.body ?? null})
+
+  return redirect('/');
+};
+
 export const loader: LoaderFunction = async () => {
   const data: LoaderData = {posts: await getPosts()}
   return json(data)
-}
+};
 
 export default function Index() {
   const { posts } = useLoaderData<LoaderData>();
+  const formData = useActionData<ActionData>();
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1 className="text-3xl text-blue-500 font-bold">Welcome to Remix</h1>
-      <ul>
-        {posts.map(post => (
-          <li key={post.title}>
-            <PostComponent header={post.title}>
-              {post.body}
-            </PostComponent>
-          </li>
-        ))}
-      </ul>
+      <h1 className="text-3xl text-blue-500 font-bold mb-5">Welcome to Remix</h1>
+      <div className="flex flex-col items-center">
+        <PostForm 
+          action="/?index" 
+          error={formData?.error} 
+          fields={formData?.fields} 
+        />
+        <ul>
+          {posts.map(post => (
+            <li key={post.title}>
+              <PostComponent header={post.title}>
+                {post.body}
+              </PostComponent>
+            </li>
+          ))}
+        </ul>
+      </div>
+      
     </div>
   );
 }
